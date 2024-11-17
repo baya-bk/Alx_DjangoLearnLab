97cf3b2bef8f764e16c2f08730d80ca05ed4dc44
Behailu <behailubedada1994@gmail.com>
2024-11-17 12:13:49 +0300

    Implement book listing view and library detail view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from . import views
+
+app_name = 'relationship_app'
 
 urlpatterns = [
-    # URL for the function-based view
-    path('books/', list_books, name='list_books'),
-    # URL for the class-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(),
+    path('books/', views.book_list, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-from .models import Book
 from django.views.generic import DetailView
-from .models import Library
+from .models import Book, Library
 
 
-def list_books(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    # Render the template with book data
-    return render(request, 'list_books.html', {'books': books})
+def book_list(request):
+    """Function-based view to list all books"""
+    books = Book.objects.all().select_related('author')
+    return render(request, 'relationship_app/list_books.html', {
+        'books': books
+    })
 
 
 class LibraryDetailView(DetailView):
+    """Class-based view to display library details"""
     model = Library
-    template_name = 'library_detail.html'  # Specify the template to use
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add all books related to the library to the context
-        context['books'] = self.object.books.all()
-        return context
+    def get_queryset(self):
+        return Library.objects.prefetch_related('books__author')
