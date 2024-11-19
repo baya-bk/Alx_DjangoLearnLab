ae626a442065d6a41fed7037ff821888a453ab7f
Behailu <behailubedada1994@gmail.com>
2024-11-19 19:10:49 +0300

    Implement book listing view and library detail view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from .views import register
+from .views import register_view
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from .views import list_books
@@ -11,7 +11,7 @@ from .views import LibraryDetailView
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('register/', register, name='register'),  # User registration
+    path('register/', register_view, name='register'),  # User registration
     path('books/', list_books, name='book_list'),  # Example protected view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
