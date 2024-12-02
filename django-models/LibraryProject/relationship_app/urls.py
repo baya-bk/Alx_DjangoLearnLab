a265a47cbee2bd0c63773661469ddeda3d0d94d9
Behailu <behailubedada1994@gmail.com>
2024-12-02 14:55:44 +0300

    Implement role-based access control and update URL patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,8 @@
 from django.contrib.auth import views as auth_views
 from . import views
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
-from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view
 
 app_name = 'relationship_app'
 
@@ -14,7 +10,14 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # User registration
     path('books/', list_books, name='book_list'),  # Example protected view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
     # Authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Role-based views
+    path('admin/', admin_view, name='admin_view'),  # Admin view URL
+    path('librarian/', librarian_view,
+         name='librarian_view'),  # Librarian view URL
+    path('member/', member_view, name='member_view'),  # Member view URL
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from .models import UserProfile
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth import views as auth_views
@@ -72,3 +74,38 @@ def register(request):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+# relationship_app/views.py
+
+
+# Check if the user is an Admin
+
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+# Check if the user is a Librarian
+
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+# Check if the user is a Member
+
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
