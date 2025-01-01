ecf204a389262b1cf5038fb2877e18eea4c36ec5
Behailu <behailubedada1994@gmail.com>
2025-01-01 14:31:59 +0300

    Add forms.py to define and manage secure form handling for the bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,24 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-
-from django import forms
-
-
-class BookSearchForm(forms.Form):
-    title = forms.CharField(max_length=255)
-
-# Example usage in a view
-
-
-def search_books(request):
-    form = BookSearchForm(request.GET)
-    if form.is_valid():
-        title = form.cleaned_data['title']
-        books = Book.objects.filter(title__icontains=title)
-    else:
-        books = Book.objects.none()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+from .forms import BookForm
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -30,22 +13,26 @@ def book_list(request):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        Book.objects.create(title=title, author=author)
-        return redirect('book_list')
-    return render(request, 'bookshelf/create_book.html')
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/create_book.html', {'form': form})
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        book.title = request.POST.get('title')
-        book.author = request.POST.get('author')
-        book.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/edit_book.html', {'book': book})
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
